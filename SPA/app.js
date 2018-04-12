6c12a0b274f915d7b3fb41cd8f26a62e7d8db8a7
清欢 <Qh@supeihuideiMac.local>
2018-04-12 11:03:17 +0800

    最基本的PHP MVC架构

--- SPA/app.js
+++
@@ -1,9 +0,0 @@
-require('./bootstrap.js')
-import Vue from 'vue'
-import router from './Router/index.js'
-
-
-new Vue({
-    el:'#app',
-    router,
-});
--- SPA/bootstrap.js
+++
@@ -1,3 +0,0 @@
-window.axios = require('axios')
-window.$ = window.jQuery = require('jquery')
-window._ = require('lodash')
